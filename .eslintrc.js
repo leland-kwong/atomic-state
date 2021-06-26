a39409e783e14f7b0463f5c1a78673e0dbaa95b4
Leland Kwong <leland.kwong@gmail.com>
2021-06-26 14:37:36 -0700

    update elint config and package vulnerabilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,24 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    "react-hooks"
-  ],
+  plugins: ['jest', '@typescript-eslint', 'react-hooks'],
   extends: [
     'airbnb',
+    'prettier',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   rules: {
     'implicit-arrow-linebreak': ['error', 'below'],
     'import/prefer-default-export': 0,
-    '@typescript-eslint/semi': 2,
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks watch dependencies
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks watch dependencies
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 0,
     'react/display-name': 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", 'tsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'tsx'] }],
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -32,37 +29,36 @@ module.exports = {
         ts: 'never',
         tsx: 'never'
       }
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'none',
+          requireLast: true
+        }
+      }
     ]
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx"
-        ]
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
       },
-      "webpack": {}
+      webpack: {}
     },
-    "import/extensions": [
-      ".js",
-      ".mjs",
-      ".jsx",
-      ".ts",
-      ".tsx"
-    ],
-    "import/parsers": {
-      "@typescript-eslint/parser": [
-        ".ts",
-        ".tsx"
-      ]
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
-    "import/core-modules": [],
-    "import/ignore": [
-      "node_modules",
-      "\\.(coffee|scss|css|less|hbs|svg|json)$"
-    ]
+    'import/core-modules': [],
+    'import/ignore': ['node_modules', '\\.(coffee|scss|css|less|hbs|svg|json)$']
+  },
+  env: {
+    'jest/globals': true
   }
-};
+}
