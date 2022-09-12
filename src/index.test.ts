a2682f67232409e29e1e53b612140f7031e1dfc0
leland-kwong <leland.kwong@gmail.com>
2022-09-12 11:14:37 -0500

    update formatting

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,69 +4,58 @@ import {
   meta,
   read,
   addWatch,
-  removeWatch,
-} from '.';
+  removeWatch
+} from '.'
 
-const add = (
-  x: number,
-  y: number,
-): number =>
-  x + y;
+const add = (x: number, y: number): number => x + y
 
 describe('atom', () => {
   test('swap', () => {
-    const count$ = atom(0);
-    const newState = swap(count$, add, 1);
+    const count$ = atom(0)
+    const newState = swap(count$, add, 1)
 
-    expect(
-      newState,
-    ).toBe(1);
-  });
+    expect(newState).toBe(1)
+  })
 
   test('meta', () => {
     const countMeta = {
-      label: 'count$',
-    };
-    const count$ = atom(0, countMeta);
+      label: 'count$'
+    }
+    const count$ = atom(0, countMeta)
 
-    expect(
-      meta(count$),
-    ).toBe(countMeta);
-  });
+    expect(meta(count$)).toBe(countMeta)
+  })
 
   test('read', () => {
-    const count$ = atom(0);
+    const count$ = atom(0)
 
-    expect(
-      read(count$),
-    ).toBe(0);
-  });
+    expect(read(count$)).toBe(0)
+  })
 
   describe('watch', () => {
     const countMeta = {
-      label: 'count$',
-    };
-    const count$ = atom(0, countMeta);
-    const watchKey = '@countWatch';
+      label: 'count$'
+    }
+    const count$ = atom(0, countMeta)
+    const watchKey = '@countWatch'
 
     test('addWatch', () => {
-      const fn = jest.fn();
+      const fn = jest.fn(() => {})
 
-      addWatch(count$, watchKey, fn);
-      swap(count$, add, 1);
+      addWatch(count$, watchKey, fn)
+      swap(count$, add, 1)
       expect(fn.mock.calls).toEqual([
-        [count$, watchKey, 0, 1],
-      ]);
-    });
+        [count$, watchKey, 0, 1]
+      ])
+    })
 
     test('removeWatch', () => {
-      const fn = jest.fn();
+      const fn = jest.fn(() => {})
 
-      addWatch(count$, watchKey, fn);
-      removeWatch(count$, watchKey);
-      swap(count$, add, 1);
-      expect(fn.mock.calls.length)
-        .toBe(0);
-    });
-  });
-});
+      addWatch(count$, watchKey, fn)
+      removeWatch(count$, watchKey)
+      swap(count$, add, 1)
+      expect(fn.mock.calls.length).toBe(0)
+    })
+  })
+})
